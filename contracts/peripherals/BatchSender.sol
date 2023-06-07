7afa2a85788b8a3d406496732120cfba579fb1dc
RevxChain <104643965+RevxChain@users.noreply.github.com>
2023-06-07 15:01:59 +0200

    Governable added

--- contracts/peripherals/BatchSender.sol
+++ contracts/peripherals/BatchSender.sol
@@ -5,8 +5,12 @@ pragma solidity 0.6.12;
 import "../libraries/token/IERC20.sol";
 import "../libraries/math/SafeMath.sol";
 
-contract BatchSender {
+import "../access/Governable.sol";
+
+contract BatchSender is Governable {
     using SafeMath for uint256;
+
+    mapping (address => bool) public isHandler;
 
     event BatchSend(
         uint256 indexed typeId,
@@ -15,11 +19,24 @@ contract BatchSender {
         uint256[] amounts
     );
 
-    function send(IERC20 _token, address[] memory _accounts, uint256[] memory _amounts) public {
+    modifier onlyHandler() {
+        require(isHandler[msg.sender], "BatchSender: forbidden");
+        _;
+    }
+
+    constructor() public {
+        isHandler[msg.sender] = true;
+    }
+
+    function setHandler(address _handler, bool _isActive) external onlyGov {
+        isHandler[_handler] = _isActive;
+    }
+
+    function send(IERC20 _token, address[] memory _accounts, uint256[] memory _amounts) public onlyHandler {
         _send(_token, _accounts, _amounts, 0);
     }
 
-    function sendAndEmit(IERC20 _token, address[] memory _accounts, uint256[] memory _amounts, uint256 _typeId) public {
+    function sendAndEmit(IERC20 _token, address[] memory _accounts, uint256[] memory _amounts, uint256 _typeId) public onlyHandler {
         _send(_token, _accounts, _amounts, _typeId);
     }
 
@@ -29,6 +46,7 @@ contract BatchSender {
             uint256 amount = _amounts[i];
             _token.transferFrom(msg.sender, account, amount);
         }
+
         emit BatchSend(_typeId, address(_token), _accounts, _amounts);
     }
 }
